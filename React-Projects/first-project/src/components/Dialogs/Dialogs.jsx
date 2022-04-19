fb7947dc24917e64c277d599692efb280888bf30
Le-Mon7 <igorejkee07@gmail.com>
2022-04-19 20:32:42 +0700

    24 lesson

--- React-Projects/first-project/src/components/Dialogs/Dialogs.jsx
+++ React-Projects/first-project/src/components/Dialogs/Dialogs.jsx
@@ -21,23 +21,36 @@ function Message(props) {
 }
 
 function Dialogs() {
+	let dialogsData = [
+		{ id: 1, name: 'Max' },
+		{ id: 2, name: 'Anastasia' },
+		{ id: 3, name: 'Jim' },
+		{ id: 4, name: 'Igor' },
+	];
+
+	let messagesData = [
+		{ id: 1, msg: 'Hi!' },
+		{ id: 2, msg: 'How are you?' },
+		{ id: 3, msg: 'Yo!' },
+		{ id: 4, msg: 'Thanks!' },
+	];
+
 	return (
 		<div className={style.dialogs}>
 			<div className={style.dialogs_items}>
 				<div className={style.dialogs_items_title}>Dialogs</div>
-				<DialogItem id='1' name='Andrey' />
-				<DialogItem id='2' name='Nastay' />
-				<DialogItem id='3' name='Jim' />
-				<DialogItem id='4' name='Igor' />
-				<DialogItem id='5' name='Kris' />
+				<DialogItem id={dialogsData[0].id} name={dialogsData[0].name} />
+				<DialogItem id={dialogsData[1].id} name={dialogsData[1].name} />
+				<DialogItem id={dialogsData[2].id} name={dialogsData[2].name} />
+				<DialogItem id={dialogsData[3].id} name={dialogsData[3].name} />
 			</div>
 			<div className={style.messages}>
 				<div className={style.messages_title}>Messages</div>
 				<div className={style.messages_items}>
-					<Message msg='Hi!' />
-					<Message msg='How are you?' />
-					<Message msg='Yo!' />
-					<Message msg='Thanks!' />
+					<Message msg={messagesData[0].msg} />
+					<Message msg={messagesData[1].msg} />
+					<Message msg={messagesData[2].msg} />
+					<Message msg={messagesData[3].msg} />
 				</div>
 			</div>
 		</div>
--- React-Projects/first-project/src/components/profile/MyPosts/MyPosts.jsx
+++ React-Projects/first-project/src/components/profile/MyPosts/MyPosts.jsx
@@ -2,6 +2,12 @@ import Post from './Post/Post';
 import style from './MyPosts.module.scss';
 
 function MyPosts() {
+	let postData = [
+		{ id: 1, msg: 'Hi! How are yoy?', likes: 5 },
+		{ id: 2, msg: 'Soy good!', likes: 11 },
+		{ id: 3, msg: "I'm funny, thanks you.", likes: 3 },
+	];
+
 	return (
 		<div className={style.posts}>
 			<h3>My post</h3>
@@ -10,9 +16,9 @@ function MyPosts() {
 				<button>Add Post</button>
 			</div>
 			<div>
-				<Post message='Hi! How are yoy?' likes='5' />
-				<Post message='Soy good!' likes='11' />
-				<Post message="I'm funny, thanks you." likes='3' />
+				<Post message={postData[0].msg} likes={postData[0].likes} />
+				<Post message={postData[1].msg} likes={postData[1].likes} />
+				<Post message={postData[2].msg} likes={postData[2].likes} />
 			</div>
 		</div>
 	);
